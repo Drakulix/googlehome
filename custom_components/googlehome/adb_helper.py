2d691d24dc2ff9e8ba1e96ebc37af74169701a0b
Victor Brekenfeld <github@drakulix.de>
2019-09-17 11:30:18 +0200

    Make adb pulling more robost

--- custom_components/googlehome/adb_helper.py
+++ custom_components/googlehome/adb_helper.py
@@ -8,20 +8,35 @@ class AdbClient():
         self.adb = adbutils.AdbClient(host=adb_host, port=adb_port)
         if adb_address is not "":
             if ":" in adb_address:
+                self.tcp = adb_address
                 print(self.adb.connect(adb_address))
                 time.sleep(3)
-                self.device = self.adb.device() 
-            else:
-                self.device = self.adb.device(serial=adb_address)
+            self.device = self.adb.device(serial=adb_address)
         else:
             self.device = self.adb.device()
         self.proto_path = proto_path
         
     def pull_proto(self):
-        print(self.device.shell(["ps", "-A", "|", "grep", "com.google.android.apps.chromecast.app", "|", "awk", "{print \$2}", "|", "su", "-c", "xargs kill"]))
-        print(self.device.shell(["monkey", "--pct-syskeys", "0", "-p", "com.google.android.apps.chromecast.app", "-c", "android.intent.category.LAUNCHER", "1"]))
+        print(self.device.shell(["su", "-c", "rm /data/data/com.google.android.apps.chromecast.app/files/home_graph*.proto"]))
         print("Sleeping...")
-        time.sleep(30)
+        time.sleep(5)
+
+        while True:
+            i = 0
+            while self.device.shell(["su", "-c", "(ls /data/data/com.google.android.apps.chromecast.app/files/home_graph*.proto >> /dev/null 2>&1 && echo yes) || echo no"]) == "no" and i < 20:
+                print(self.device.shell(["ps", "-A", "|", "grep", "com.google.android.apps.chromecast.app", "|", "awk", "{print \$2}", "|", "su", "-c", "xargs kill"]))
+                time.sleep(5)
+                print(self.device.shell(["monkey", "--pct-syskeys", "0", "-p", "com.google.android.apps.chromecast.app", "-c", "android.intent.category.LAUNCHER", "1"]))
+                time.sleep(10)
+                i += 1
+            if self.device.shell(["su", "-c", "(ls /data/data/com.google.android.apps.chromecast.app/files/home_graph*.proto >> /dev/null 2>&1 && echo yes) || echo no"]) == "no":
+                self.device.shell(["reboot"])
+                time.sleep(20)
+                if self.tcp:
+                    print(self.adb.connect(self.tcp))
+            else:
+                break
+
         print(self.device.shell(["su", "-c", "cp /data/data/com.google.android.apps.chromecast.app/files/home_graph*.proto /sdcard/home_graph.proto"]))
         self.device.sync.pull("/sdcard/home_graph.proto", self.proto_path)
         return io.FileIO(self.proto_path)
